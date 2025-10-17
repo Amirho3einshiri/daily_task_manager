2acd4c54e5f2a1ac390ab6432d1e730cb6593a1f
Amirho3einShiri <amirhoseinshiri2008@gmail.com>
2025-10-18 00:10:19 +0330

    initial commit

---
+++ task_manager.py
@@ -0,0 +1,15 @@
+# پروژه: مدیریت وظایف روزانه
+# نویسنده: امیرحسین شیری
+
+def add_task(task):
+    pass  # TODO: اضافه کردن وظیفه به لیست
+
+def remove_task(task):
+    pass  # TODO: حذف وظیفه از لیست
+
+def list_tasks():
+    pass  # TODO: نمایش همه وظایف
+
+if __name__ == "__main__":
+    # TODO: منوی اصلی برنامه
+    pass
